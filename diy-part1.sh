db5f8070e2fd84484f6731ec3cd39b9a383c71a3
facerleo <86334632+facerleo@users.noreply.github.com>
2021-08-17 20:41:34 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,6 +8,7 @@
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default                                  #kenzok8软件库
 # echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default                                             #kenzok8软件库依赖
 echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default                           #易有云 luci-app-ddnsto和luci-app-linkease
+echo 'patch -p1 < add-patch_dts_file-wifi-xradio.patch' >> feeds.conf.default
 
 git clone https://github.com/esirplayground/luci-app-poweroff.git package/lean/luci-app-poweroff                       #关机
 # git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/lean/luci-theme-edge                      #主题-edge-动态登陆界面
@@ -17,4 +18,3 @@ git clone https://github.com/mitsukileung/luci-app-filebrowser.git package/lean/
 git clone https://github.com/small-5/luci-app-adblock-plus.git package/lean/luci-app-adblock-plus                      #需取消勾选base-system->dnsmasq
 # git clone -b master https://github.com/vernesong/OpenClash.git package/lean/luci-app-openclash                         #openclash
 # git clone https://github.com/tty228/luci-app-serverchan.git package/lean/luci-app-serverchan                           #微信推送
-git clone https://github.com/melsem/openwrt-lede_xradio-xr819_soc-audio.git lede/xr819
